496e778b23c77b6b1f8841fd52a1f06a9b09db97
duchang <duchang.dev@gmail.com>
2021-05-26 23:57:21 +0900

    feat: Add login step [WIP] auth 컨텍스트를 이용해서 로그인 구현 예정

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,15 +1,28 @@
 import Layout from '@/components/Layout';
+import Link from 'next/link';
+import { useAuthState, useAuthDispatch } from '@/contexts/auth.context';
+import React from 'react';
 
-const LoginPage = () => (
-  <Layout title="calendar - 로그인">
-    <section>
-      <div>Login</div>
-      <form>
-        <input type="text" name="id" id="id" />
-        <input type="password" name="password" id="password" />
-      </form>
-    </section>
-  </Layout>
-);
+const LoginPage = () => {
+  const handleLoginFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Layout title="calendar - 로그인">
+      <section>
+        <div>Login</div>
+        <form onSubmit={handleLoginFormSubmit}>
+          <input type="text" name="id" id="id" />
+          <input type="password" name="password" id="password" />
+          <button>Login</button>
+        </form>
+        <Link href="/signup">
+          <a>Sign up</a>
+        </Link>
+      </section>
+    </Layout>
+  );
+};
 
 export default LoginPage;
