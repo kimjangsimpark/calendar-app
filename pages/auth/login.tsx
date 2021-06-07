442807b7026a8b4631974a8037c0f4698a2ef054
duchang <duchang.dev@gmail.com>
2021-06-07 23:29:29 +0900

    feat: Publish login page UI

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '@/components/Layout';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useAuthState, useAuthDispatch } from '@/contexts/auth.context';
 import { User } from '@/interfaces';
+import loginStyle from '@/styles/login.module.scss';
 
 const LoginPage = () => {
   const router = useRouter();
@@ -43,29 +44,37 @@ const LoginPage = () => {
 
   return (
     <Layout title="calendar - 로그인">
-      <section>
-        <div>Login</div>
-        <form onSubmit={handleLoginFormSubmit}>
+      <section className={loginStyle.loginContainer}>
+        <article>
+          <h2 className={loginStyle.title}>로그인</h2>
+          <div className={loginStyle.welcomeMessageWrapper}>
+            <p>환영합니다.</p>
+            <p>로그인을 부탁드려요!</p>
+          </div>
+        </article>
+        <form onSubmit={handleLoginFormSubmit} className={loginStyle.loginForm}>
           <input
+            className={loginStyle.input}
             type="text"
             name="id"
             id="id"
-            placeholder="아이디를 입력하세요"
+            placeholder="ID (email)"
             value={id}
             onChange={handleChange}
           />
           <input
+            className={loginStyle.input}
             type="password"
             name="password"
             id="password"
-            placeholder="비밀번호를 입력하세요"
+            placeholder="Password"
             value={password}
             onChange={handleChange}
           />
-          <button>Login</button>
+          <button className={loginStyle.loginButton}>Login</button>
         </form>
         <Link href="/signup">
-          <a>Sign up</a>
+          <a className={loginStyle.signupLink}>Sign up</a>
         </Link>
       </section>
     </Layout>
