e65bd39afd970a12dd42af308ec342fb5989c5f6
winetree94 <winetree94@outlook.com>
2021-06-30 21:43:28 +0900

    fix: jwt module injection problem

--- src/server/auth/auth.module.ts
+++ src/server/auth/auth.module.ts
@@ -3,9 +3,14 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({ secret: 'testtesttesttest' }),
+  ],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
