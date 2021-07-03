b19101f073bd773cf7013bdc032040ee59d0eab9
winetree94 <winetree94@outlook.com>
2021-07-03 18:51:36 +0900

    feat: type declaration

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,25 +1,36 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
+/* eslint-disable no-unused-vars */
+import IndexComponent from 'kjsp-calendar-core';
+
+/* eslint-disable no-unused-vars */
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'kjsp-index': IndexComponent;
+    }
+  }
+}
 
 interface PropTypes {
-    width: number;
-    height: number;
+  width: number;
+  height: number;
 }
 
-const Calendar = ({width, height}: PropTypes) => {
-    useEffect(() => {
-        require('kjsp-calendar-core');
-    }, [])
+const Calendar = ({ width, height }: PropTypes) => {
+  useEffect(() => {
+    require('kjsp-calendar-core');
+  }, []);
 
-    const calendarWrapperStyle = {
-        width: `${width}px`,
-        height: `${height}px`
-    }
+  const calendarWrapperStyle = {
+    width: `${width}px`,
+    height: `${height}px`,
+  };
 
-    return (
-        <div style={calendarWrapperStyle}>
-            <kjsp-index />
-        </div>
-    )
-}
+  return (
+    <div style={calendarWrapperStyle}>
+      <kjsp-index />
+    </div>
+  );
+};
 
 export default Calendar;
